b4df24e798e001fa9535275fb793d38b611595d1
Jafeht Soturno <jasordev@gmail.com>
2022-06-01 23:29:49 -0400

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,108 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- Main container -->
+  <div class="container mx-auto p-4">
+    <!-- Main box -->
+    <div class="w-full bg-gray-100 shadow-lg p-4 rounded-lg">
+      <h1 class="mb-5 text-4xl text-center">IdeaBox</h1>
+
+      <!-- Add Idea -->
+      <AddIdea
+        :user="user"
+        @do-login="doLogin"
+        @do-logout="doLogout"
+        @add-idea="addIdea"
+      />
+      <!-- End of Add Idea -->
+
+      <!-- Idea item -->
+      <AppIdea v-for="(idea, $index) in ideas" :key="$index" :idea="idea" />
+      <!-- End of Idea item -->
+    </div>
+    <!-- End of main box -->
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import AppIdea from "@/components/AppIdea.vue";
+// import seed from "@/seed.json";
+import { ref } from "vue";
+import { auth, db, firebase } from "@/firebase";
+import AddIdea from "./components/AddIdea.vue";
 export default {
-  name: 'App',
+  name: "App",
+  setup() {
+    const ideas = ref([]);
+
+    let user = ref(null);
+
+    auth.onAuthStateChanged(async (auth) => (user.value = auth ? auth : null));
+
+    db.collection("ideas").onSnapshot(
+      (snapshot) => {
+        const newIdeas = [];
+        snapshot.docs.forEach((doc) => {
+          let { name, user, userName, votes } = doc.data();
+          let id = doc.id;
+          newIdeas.push({
+            name,
+            user,
+            userName,
+            votes,
+            id,
+          });
+        });
+        ideas.value = newIdeas;
+      },
+      (error) => console.error(error)
+    );
+
+    const doLogin = async () => {
+      const provider = new firebase.auth.GoogleAuthProvider();
+
+      try {
+        await auth.signInWithPopup(provider);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    const doLogout = async () => {
+      try {
+        await auth.signOut();
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    const addIdea = async (data) => {
+      try {
+        await db.collection("ideas").add({
+          name: data.value,
+          user: user.value.uid,
+          userName: user.value.displayName,
+          votes: 0,
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    return { ideas, user, doLogin, doLogout, addIdea };
+  },
   components: {
-    HelloWorld
-  }
-}
+    AppIdea,
+
+    AddIdea,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.user-actions {
+  @apply mt-2 text-center;
+}
+
+.user-actions a {
+  font-weight: bold;
+  text-decoration: underline;
 }
 </style>
---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+import firebase from "firebase/app";
+
+import "firebase/auth";
+import "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCKEOwni-tOPmAl5pu5BtUF4Fc4Qt6Okk4",
+  authDomain: "ideadboxlive.firebaseapp.com",
+  projectId: "ideadboxlive",
+  storageBucket: "ideadboxlive.appspot.com",
+  messagingSenderId: "388166670609",
+  appId: "1:388166670609:web:4de284b64f6a8e53d5d4b0"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const db = firebase.firestore();
+
+export{ auth, firebase, db }
